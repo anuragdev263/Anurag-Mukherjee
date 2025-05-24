774359562e61156e8a367c1ca4b5ac71f90a6cc2
anuragdev263 <2405263@kiit.ac.in>
2025-05-24 18:45:37 +0000

    container config

---
+++ 01_basics/add2numbers.js
@@ -0,0 +1,7 @@
+function addTwoNumbers(num1,num2){
+    return num1+num2;
+}
+let number1=5;
+let number2=10;
+let sum=addTwoNumbers(number1,number2);
+console.log("The sum of "+number1 +" and "+number2+" is: "+sum);
