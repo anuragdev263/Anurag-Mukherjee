a76f987efcd507082d0a429c24bb9754b23ea6f8
anuragdev263 <2405263@kiit.ac.in>
2025-05-26 07:31:59 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=2405263;
+let accountEmail="anurag@gmail.com";
+let accountPassword="12345";
+let accountCity1="Bengaluru";
+let accountCity2;
+console.log("Your account ID is =: "+accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity1,accountCity2]);
